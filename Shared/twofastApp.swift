9dc9343b25b848ab1d9ab1e6c847873c65339edd
Noah Rubin <nrubin29@gmail.com>
2020-07-07 00:33:59 -0400

    Initial Commit

---
+++ Shared/twofastApp.swift
@@ -0,0 +1,17 @@
+//
+//  twofastApp.swift
+//  Shared
+//
+//  Created by Noah Rubin on 7/7/20.
+//
+
+import SwiftUI
+
+@main
+struct twofastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
